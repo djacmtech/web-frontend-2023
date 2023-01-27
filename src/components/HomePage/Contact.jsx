dff6b2dee91853d52cf879b7f782240cc200b3a2
talkshrey <talkshrey@gmail.com>
2023-01-27 17:55:47 +0530

    contact us changed

--- src/components/HomePage/Contact.jsx
+++ src/components/HomePage/Contact.jsx
@@ -3,93 +3,70 @@ import instagram from "../../assets/icons/instagram.svg";
 import facebook from "../../assets/icons/facebook.svg";
 import linkedin from "../../assets/icons/linkedin.svg";
 import direction from "../../assets/icons/direction_icon.svg";
+import "../../styles/Contact.css";
 
 const Contact = () => {
   return (
-    <section className='contact'>
-      <h1 className='head'>GET IN TOUCH</h1>
+    <section className="contact">
+      <h1 className="head">GET IN TOUCH</h1>
 
-      <div className='container'>
-        <form action='#'>
-          <table>
-            <tr>
-              <td>
-                <div className='up'>
-                  <div className='design1'>
-                    <input type='text' placeholder='Name' />
+      <div className="container">
+        <form action="#">
+          <div className="outer-table">
+            <div className="outer-col">
+              <div className="design1">
+                <input type="text" placeholder="name" />
+                <input type="text" placeholder="email" />
+              </div>
+              <div className="inner-table">
+                <div className="inner-col">
+                  <div className="design2">
+                    <a href="coreacm@gmail.com" className="email">
+                      coreacm@gmail.com
+                    </a>
                   </div>
-
-                  <div className='design2'>
-                    <input type='text' placeholder='Email' />
+                  <div className="soc">
+                    <div className="design3">
+                      <a href="https://www.facebook.com/djscoeacm/">
+                        <img src={facebook} width={50} alt="facebook" />
+                      </a>
+                    </div>
+                    <div className="design3">
+                      <a href="https://www.instagram.com/djsanghvi_acm/">
+                        <img src={instagram} width={50} alt="instagram" />
+                      </a>
+                    </div>
                   </div>
-                  <table>
-                    <tr>
-                      <td>
-                        <div className='ref'>
-                          <div className='design3'>
-                            <a href='coreacm@gmail.com'>
-                              <p className='p12'>
-                                <b>coreacm@gmail.com</b>
-                              </p>
-                            </a>
-                          </div>
-                          <div className='design4'>
-                            <p className='p1'>Rishi Doshi:+91 1111111111</p>
-                          </div>
-                        </div>
-                      </td>
-                      <td>
-                        <div className='social'>
-                          <div className='soc1'>
-                            <div className='design5'>
-                              <a href='https://www.facebook.com/djscoeacm/'>
-                                <img src={facebook} alt='facebook' />
-                              </a>
-                            </div>
-
-                            <div className='design6'>
-                              <a href='https://www.instagram.com/djsanghvi_acm/'>
-                                <img src={instagram} alt='instagram' />
-                              </a>
-                            </div>
-                          </div>
-
-                          <br />
-                          <div className='soc2'>
-                            <div className='design7'>
-                              <a href='https://www.linkedin.com/dj-sanghvi-acm/'>
-                                <img src={linkedin} alt='linkedin' />
-                              </a>
-                            </div>
-
-                            <div className='design8'>
-                              <a href='https://goo.gl/maps/3pkAhj2jvCGia6XD8'>
-                                <img src={direction} alt='direction' />
-                              </a>
-                            </div>
-                          </div>
-                        </div>
-                      </td>
-                    </tr>
-                  </table>
                 </div>
-              </td>
-
+                <div className="inner-col">
+                  <div className="design2">
+                    <p className="p1">+91 9833447698</p>
+                  </div>
+                  <div className="soc">
+                    <div className="design3">
+                      <a href="https://www.linkedin.com/dj-sanghvi-acm/">
+                        <img src={linkedin} width={50} alt="linkedin" />
+                      </a>
+                    </div>
+                    <div className="design3">
+                      <a href="https://goo.gl/maps/3pkAhj2jvCGia6XD8">
+                        <img src={direction} width={50} alt="direction" />
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="outer-col">
+              <div className="design4">
+                <textarea type="text" placeholder="message" />
+              </div>
               <br />
-              <td>
-                <div className='bottom'>
-                  <div className='design11'>
-                    <textarea type='text' placeholder='Message' />
-                  </div>
-                  <br />
-
-                  <div className='design10'>
-                    <button>Send </button>
-                  </div>
-                </div>
-              </td>
-            </tr>
-          </table>
+              <div className="design5">
+                <button> Send </button>
+              </div>
+            </div>
+          </div>
         </form>
       </div>
     </section>
